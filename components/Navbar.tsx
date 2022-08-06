26049332a9ec0f28f1f7a10b087c1de3e8997605
Tim Pollmann <pollmann.tim@protonmail.com>
2022-08-06 20:38:52 +0200

    first navbar implementation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Navbar.module.css';
+import logo from '../public/logo.png';
+
+interface LinkWrapperProps {
+  path: string,
+  text: string,
+  currentPath: string,
+}
+
+function LinkWrapper(props: LinkWrapperProps) {
+  const { path, text, currentPath } = props;
+  return (
+    <li className={`${styles.linkWrapper} ${currentPath === path ? styles.selected : null}`}>
+      <Link className={styles.link} href={path}>{text}</Link>
+    </li>
+  );
+}
 
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <h1 className={styles.header}>Navbar</h1>
+    <div className={styles.container}>
+      <Link href="/">
+        <a>
+          <div className={styles.logoWrapper}>
+            <Image src={logo} layout="responsive" />
+          </div>
+        </a>
+      </Link>
+      <ul className={styles.linkContainer}>
+        <LinkWrapper path="/" text="Aktuelles" currentPath={router.pathname} />
+        <LinkWrapper path="/help" text="Helfen" currentPath={router.pathname} />
+      </ul>
+    </div>
   );
 }
---
+++ pages/help.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Home.module.css';
+
+export default function Help() {
+  return (
+    <h1 className={styles.header}>Help</h1>
+  );
+}
