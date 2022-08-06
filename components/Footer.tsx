d2405cbdfd2905c4be359dc54535a84e3e37a155
Tim Pollmann <pollmann.tim@protonmail.com>
2022-08-06 17:55:03 +0200

    added navbar and footer, added css-reset to globals.css

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+  return (
+    <h1 className={styles.header}>Footer</h1>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <h1 className={styles.header}>Navbar</h1>
+  );
+}
