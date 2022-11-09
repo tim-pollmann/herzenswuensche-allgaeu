889aeecbab55afa4dd1779c5223ec52ad2e314f2
Tim Pollmann <pollmann.tim@protonmail.com>
2022-11-09 17:05:16 +0100

    added impressum for vercel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,17 @@ export default function Footer() {
   return (
     <footer className={styles.container}>
       <div className={styles.contentContainer}>
-        <h1>Footer</h1>
-        <p>ksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks skksjhlkjahsölsjhdkölj jkasHKH jlskh lskjfkljsklöjö skujhjkj siks sk</p>
+        <h1>Impressum</h1>
+        <p>Herzenswünsche Allgäu e.V.</p>
+        <p>Im Oberösch 2</p>
+        <p>87437 Kempten</p>
+        <p>Vereinsregister: VR201160</p>
+        <p>Registergericht: Kempten (Allgäu)</p>
+        <p>Vertreten durch den Vorstand:</p>
+        <p>Vorsitzende: Andrea Portsidis</p>
+        <p>stellv. Vorsitzender: Markus Treffler</p>
+        <p>Telefon: 0176 15127413</p>
+        <p>E-Mail: andrea@herzenswuensche-allgaeu.de</p>
       </div>
     </footer>
   );
